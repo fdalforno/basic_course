1784182f1898b9a9a7656655f5b2cf9a33ffbc2e
Fabio Dal Forno <Fabio.DalForno@CattolicaIts.it>
2021-02-05 18:43:17 +0100

    Gestione Algoritmi

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Qui sotto l'elenco delle lezioni:
 + [Introduzione](https://colab.research.google.com/github/fdalforno/basic_course/blob/main/01%20-%20Base.ipynb)
 + [Numpy](https://colab.research.google.com/github/fdalforno/basic_course/blob/main/02%20-%20Numpy.ipynb)
 + [Pandas](https://colab.research.google.com/github/fdalforno/basic_course/blob/main/03%20-%20Pandas.ipynb)
++ [Matplotlib](https://colab.research.google.com/github/fdalforno/basic_course/blob/main/04%20-%20Matplotlib.ipynb)
++ [Algoritmi](https://colab.research.google.com/github/fdalforno/basic_course/blob/main/05%20-%20Algoritmi.ipynb)
 
 Le lezioni prendono spunto dal libro di Sebastian Raschka (python machine learning)
--- libs/dataset.py
+++ libs/dataset.py
@@ -68,5 +68,32 @@ def plot_decision_region(X,y,classifier,resolution=0.02):
     plt.legend(loc="upper left")
 
 
+def plot_decision_region_proba(X,y,classifier,resolution=0.02):
+
+    cm = plt.cm.RdBu
+    cm_bright = ['#FF0000', '#0000FF']
+
+    classes = np.unique(y)
+    markers = ('s','x')
+
+    x1_min,x1_max = X[:,0].min() - 1,X[:,0].max() + 1
+    x2_min,x2_max = X[:,1].min() - 1,X[:,1].max() + 1
+    xx1,xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+
+    Z = classifier.predict_proba(np.array([xx1.ravel(), xx2.ravel()]).T)[:, 1]
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cm)
+
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+
+    for idx,cl in enumerate(classes):
+        plt.scatter(x=X[y==cl,0],y=X[y==cl,1],alpha=0.8,c=cm_bright[idx],marker=markers[idx],label=cl)
+
+    plt.legend(loc="upper left")
+
 
 
